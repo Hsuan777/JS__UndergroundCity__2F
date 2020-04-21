c8e8c31a40b23da5781b685e15484b417af23f17
syuan0212@gmail.com <hsuan777>
2020-04-21 23:09:49 +0800

    修改__時針轉動角度

--- js/all.js
+++ js/all.js
@@ -1,11 +1,4 @@
 
-// 每分每秒各轉 6度
-// 每一小時轉 30度
-// 每 60分鐘，時針轉 30/60  = 0.5度
-// 每秒鐘，時針轉 30/360 = 0.00833333度
-// 每 60秒鐘，分針轉 6度
-// 每秒鐘，分針轉 6/60 0.1度
-
 function clock() {
     // 取得使用者本地時間;
     // let currentTime__origin = Date();
@@ -15,8 +8,8 @@ function clock() {
     let currentTime__hour = currentTime.getHours();
 
     let angel__second = currentTime__second * 6;
-    let angel__minute = (currentTime__minute * 6) + (currentTime__second * 0.1);
-    let angel__hour   = (currentTime__hour * 30) + (currentTime__minute * 0.5) + (currentTime__second * 0.00833333);
+    let angel__minute = (currentTime__minute * 6) + (currentTime__second * 6/60);
+    let angel__hour   = (currentTime__hour * 30) + (currentTime__minute * 0.5) ;
 
     let clockSecond = document.querySelector('.clock__second');
     let clockMinute = document.querySelector('.clock__minute');
@@ -32,10 +25,9 @@ function clock() {
 }
 
 // setInterval
-// 每 250毫秒跑一次
-// setInterval(clock,0);
+// setInterval(clock,1000);
 
 
 // requestAnimationFrame 
-// 根據使用者瀏覽器刷新頻率
+// 根據使用者瀏覽器刷新頻率去更新畫面
 requestAnimationFrame(clock);
